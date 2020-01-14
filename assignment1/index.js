6f539a8eef2a03dff7fb9dff0b61bc2475cb6db8
Fredrik E. Skjelvik <fredrikerichsen16@gmail.com>
2020-01-14 14:49:54 +0100

    Made table sortable

--- assignment1/index.js
+++ assignment1/index.js
@@ -1,5 +1,29 @@
 $('table.sortable th').on('click', function() {
-    let column = $(this).data('column');
+    let column = $('table.sortable th').index(this);
+    if(column === 0) return; // don't allow sorting on image column
 
-    console.log(column);
+    $rows = [];
+    $('table.sortable tbody tr').each(function() {
+        $rows.push($(this));
+    });
+
+    // console.log($rows);
+
+    $rows.sort(function($a, $b) {
+        let a = $a.children().eq(column).text();
+        let b = $b.children().eq(column).text();
+
+        // console.log(a, b);
+
+        return a.localeCompare(b);
+    });
+
+    let $tbody = $('table.sortable tbody');
+
+    $tbody.empty();
+    for(let i = 0; i < $rows.length; i++) {
+        $tbody.append($rows[i]);
+    }
+
+    // console.log($rows);
 });
