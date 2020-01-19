a84ee4ae9b91ce47253db6480c7897016f61e5de
Fredrik E. Skjelvik <fredrikerichsen16@gmail.com>
2020-01-19 21:07:15 +0100

    3)

--- assignment1/index.js
+++ assignment1/index.js
@@ -1,6 +1,6 @@
 $('table.sortable th').on('click', function() {
     let column = $('table.sortable th').index(this);
-    if(column === 0) return; // don't allow sorting on image column
+    if (column === 0) return; // don't allow sorting on image column
 
     $rows = [];
     $('table.sortable tbody tr').each(function() {
@@ -21,7 +21,7 @@ $('table.sortable th').on('click', function() {
     let $tbody = $('table.sortable tbody');
 
     $tbody.empty();
-    for(let i = 0; i < $rows.length; i++) {
+    for (let i = 0; i < $rows.length; i++) {
         $tbody.append($rows[i]);
     }
 
@@ -50,28 +50,25 @@ $('form.add-phone-form').on('submit', function(e) {
     });
 });
 
+$(".reset").click(function() {
+    $.get("https://wt.ops.labs.vu.nl/api20/78c5681b/reset", function() {
 
-
-
-
-$(document).ready(function(){
-  $(".reset").click(function(){
-    $.get("https://wt.ops.labs.vu.nl/api20/78c5681b/reset", function(){
     });
-  });
 });
 
-function addRow(obj){
-  var table = "<tr><td>" + obj.brand + "</td> <td>" + obj.model + "</td> <td>" + obj.os + "</td> <td><img src=" + obj.image + "></td> <td>" + obj.screensize + "</td></tr>";
-  document.getElementById("bodyTable").innerHTML += table;
+function addRow(obj) {
+    var table = "<tr><td><img src=" + obj.image + "></td> <td>" + obj.brand + "</td> <td>" + obj.model + "</td> <td>" + obj.os + "</td> <td>" + obj.screensize + "</td></tr>";
+    document.getElementById("bodyTable").innerHTML += table;
 }
 
-function getData(){
-    $.get("https://wt.ops.labs.vu.nl/api20/78c5681b", function(data){
-      for(i = 0; i< data.lenght; i++){
-        addRow(data[i]);
-      }
+function getData() {
+    $.get("https://wt.ops.labs.vu.nl/api20/78c5681b", function(data) {
+        console.log(data);
+        for (i = 0; i < data.length; i++) {
+            addRow(data[i]);
+        }
+        console.log(document.getElementById("bodyTable").innerHTML);
     }, "json");
-
-
 }
+
+getData();
